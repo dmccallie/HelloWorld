a548bc2ba24b858cca1b1a8da7359b78de73ee29
David McCallie <dmccallie@gmail.com>
2015-05-05 17:33:50 -0500

    first branch experiment

--- helloworld/hello.py
+++ helloworld/hello.py
@@ -4,5 +4,6 @@ def getWorld():
 	#this is not the one I want
 	return "wrong world!"
 
-print "hello sweet old " + world.getWorld()
+print "hello sweet old " + world.getWorld() 
+print "hello from branch_1!"
 
